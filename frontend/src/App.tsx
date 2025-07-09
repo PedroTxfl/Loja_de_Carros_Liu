a458f9d3e8eba59deb24285ba42233f8e79d2794
Pedro Tian <pedro.tx2001@gmail.com>
2025-07-08 22:36:07 -0300

    Inicia front-end com React e configura CORS no back-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,35 @@
-import { useState } from 'react';
+// client/src/App.tsx
+import { Routes, Route, Link } from 'react-router-dom';
 import { GerenciarVeiculos } from './pages/GerenciarVeiculos';
 import { GerenciarClientes } from './pages/GerenciarClientes';
 import { GerenciarVendas } from './pages/GerenciarVendas';
 import { GerenciarUsuarios } from './pages/GerenciarUsuarios';
-import { GaleriaPage } from './pages/GaleriaPage'; 
+import { GaleriaPage } from './pages/GaleriaPage';
 import './App.css';
 
 function App() {
-    const [pagina, setPagina] = useState<'galeria' | 'veiculos' | 'clientes' | 'vendas' | 'usuarios'>('galeria');
-
     return (
         <div className="App">
             <header className="App-header">
                 <nav>
-                    <button onClick={() => setPagina('galeria')}>Galeria</button> 
-                    <button onClick={() => setPagina('veiculos')}>Gerir Veículos</button>
-                    <button onClick={() => setPagina('clientes')}>Gerir Clientes</button>
-                    <button onClick={() => setPagina('usuarios')}>Gerir Utilizadores</button>
-                    <button onClick={() => setPagina('vendas')}>Registar Venda</button>
+                    <Link to="/"><button>Galeria</button></Link>
+                    <Link to="/veiculos"><button>Gerir Veículos</button></Link>
+                    <Link to="/clientes"><button>Gerir Clientes</button></Link>
+                    <Link to="/usuarios"><button>Gerir Utilizadores</button></Link>
+                    <Link to="/vendas"><button>Registar Venda</button></Link>
                 </nav>
                 <hr />
-                {pagina === 'galeria' && <GaleriaPage />} 
-                {pagina === 'veiculos' && <GerenciarVeiculos />}
-                {pagina === 'clientes' && <GerenciarClientes />}
-                {pagina === 'usuarios' && <GerenciarUsuarios />}
-                {pagina === 'vendas' && <GerenciarVendas />}
+
+                <Routes>
+                    <Route path="/" element={<GaleriaPage />} />
+                    <Route path="/veiculos" element={<GerenciarVeiculos />} />
+                    <Route path="/clientes" element={<GerenciarClientes />} />
+                    <Route path="/usuarios" element={<GerenciarUsuarios />} />
+
+                    <Route path="/vendas" element={<GerenciarVendas />} />
+
+                    <Route path="/vendas/veiculo/:id" element={<GerenciarVendas />} />
+                </Routes>
             </header>
         </div>
     );
